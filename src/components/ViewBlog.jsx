4f3f2bfb804dc80c1b8dcdc52b147d42b4b050da
siddhant shrivastava <siddhant.shrivastava@mountblue.io>
2018-08-03 13:08:48 +0530

    for production

--- src/components/ViewBlog.jsx
+++ src/components/ViewBlog.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Header from "./Header";
 import { Link } from "react-router-dom";
-import {DeleteAction,GetAction} from '../actions/action'
+import {DeleteAction} from '../actions/action'
 class ViewBlog extends Component {
   
   render() {
